39761230bb4478299741a452e38d325b77382903
Venkat Mamidi <c.venkat.m@levadata.com>
2023-03-23 17:24:06 +0000

    m

--- src/test/java/readFromWeb.java
+++ src/test/java/readFromWeb.java
@@ -1,6 +1,7 @@
 import java.awt.AWTException;
 import java.io.IOException;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -14,7 +15,10 @@ public class readFromWeb {
 		String userdir = System.getProperty("user.dir");
 		System.setProperty("webdriver.chrome.driver", userdir + "/src/test/resources/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		driver.manage().window().maximize();	
-		driver.get("https://www.eenadu.net/");
+		driver.manage().window().maximize();
+		driver.get("https://www.eenadu.net/telugu-news/sports/rohit-sharma-said-ipl-franchises-now-own-players-they-have-to-manage-workload-in-world-cup-year/0400/123051156");
+
+		String str = driver.findElement(By.xpath("//div[@class='two-col-left-block box-shadow telugu_uni_body fullstory']")).getText();
+		System.out.println(str);
 }
 }
