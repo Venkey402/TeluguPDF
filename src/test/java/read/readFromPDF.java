fa3d95453718f93d421da7048327d0bc918b5b41
Venkat Mamidi <c.venkat.m@levadata.com>
2023-03-23 19:18:19 +0000

    .

--- src/test/java/read/readFromPDF.java
+++ src/test/java/read/readFromPDF.java
@@ -4,11 +4,14 @@ import java.io.File;
 import java.io.IOException;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.Test;
@@ -18,40 +21,53 @@ import com.mysql.jdbc.Statement;
 
 public class readFromPDF {
 
+	Statement stmt;
+
+	String userdir ;
+
+	WebDriver driver ;
+
 	@Test
 	public void insertPDFtoDB() throws IOException, SQLException
 	{
 		String filename = "teluguenglishdic0000revp";
-		
+
 		String textFrpmPDF =readPDF(filename);
 		Statement stmt = createDBconnection();
 		paragraphToWords(textFrpmPDF,stmt);
 	}
 
+	
 	@Test
 	public void readfromweb() throws AWTException, InterruptedException, IOException, SQLException
 	{
-
-
-		Statement stmt = createDBconnection();
-
-		String userdir = System.getProperty("user.dir");
+		stmt = createDBconnection();
+		userdir = System.getProperty("user.dir");
 		System.setProperty("webdriver.chrome.driver", userdir + "/src/test/resources/chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
+		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		DesiredCapabilities capabilities = new DesiredCapabilities();
 		capabilities.setCapability("acceptInsecureCerts", "true");
-		driver.get("https://www.andhrajyothy.com/2023/andhra-pradesh/ananthapuram/guttipendekallu-doubling-line-with-rs3518-crores-1035358.html");
-		String textFrpmWeb = driver.findElement(By.xpath("//*[@class='articleBodyCont']")).getText();
-		
-		
-		//String textFrpmWeb = driver.findElement(By.xpath("//div[@class='two-col-left-block box-shadow telugu_uni_body fullstory']")).getText();
-		System.out.println(textFrpmWeb);
-		paragraphToWords(textFrpmWeb,stmt);
+		driver.get("https://www.andhrajyothy.com/andhra-pradesh");
+		List<WebElement> urls_webelements = driver.findElements(By.xpath("//a[contains(@href,'.html')]"));
+		ArrayList<String> urls_string = new  ArrayList<String>();
+		for(WebElement we:urls_webelements)
+		{
+			urls_string.add(we.getAttribute("href"));
+		}
+		for(String st:urls_string)
+		{
+			driver.get(st);
+			String textFrpmWeb = driver.findElement(By.xpath("//*[@class='articleBodyCont']")).getText();
+
+			//String textFrpmWeb = driver.findElement(By.xpath("//div[@class='two-col-left-block box-shadow telugu_uni_body fullstory']")).getText();
+			System.out.println(textFrpmWeb);
+			paragraphToWords(textFrpmWeb,stmt);
+		}
 	}
 
-	
-	
+
+
 	public String readPDF(String filename) throws IOException
 	{
 		PDDocument doc =PDDocument.load(new File(System.getProperty("user.dir")+"/src/test/resources/"+filename+".pdf"));
@@ -82,5 +98,10 @@ public class readFromPDF {
 			}
 		}
 	}
+
+//	public void navigateAndrajyothi()
+//	{
+//		driver.findElement(By.xpath(""))
+//	}
 
 }
